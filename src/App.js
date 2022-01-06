630cef554dcaf9d377cad34634a4aacb015b783f
Korey Cheshire <korey.cheshire@gmail.com>
2022-01-06 12:08:23 -0800

    built App.js

--- src/App.js
+++ src/App.js
@@ -1,2 +1,22 @@
-import React from "react";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import About from "./components/about/About";
+import Portfolio from "./components/project/Project";
+import Resume from "./components/resume/Resume";
+import Contact from "./components/contact/Contact";
 
+
+const App = () => {
+    return (
+      <div className="App">
+        <Header />
+        <About />
+        <Portfolio />
+        <Contact />
+        <Resume />
+        <Footer />
+      </div>
+    );
+  };
+  
+  export default App;
